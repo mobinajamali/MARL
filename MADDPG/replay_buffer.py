36c0ed4a2a9445687be6a851f92d64556765f6fb
mobinajamali <mobina.jamali.mj@gmail.com>
2024-08-12 18:16:47 -0600

    ready function

--- MADDPG/replay_buffer.py
+++ MADDPG/replay_buffer.py
@@ -3,19 +3,19 @@ import numpy as np
 class Replay:
     '''
     responsible to store a global critic memory transition and local actor
-    memory transition for each actor
+    memory transition for each actor (numpy arrays)
     '''
-    def __init__(self, mem_size, actor_dim, critic_dim, n_actions, n_agents, batch_size):
+    def __init__(self, mem_size, actor_dims, critic_dims, n_actions, n_agents, batch_size):
         self.mem_size = mem_size
         self.mem_cntr = 0
-        self.actor_dim = actor_dim
-        self.critic_dim = critic_dim
+        self.actor_dims = actor_dims
+        self.critic_dims = critic_dims
         self.n_agents = n_agents
         self.batch_size = batch_size
         self.n_actions = n_actions
 
-        self.state_mem = np.zeros((self.mem_size, self.critic_dim))
-        self.new_state_mem = np.zeros((self.mem_size, self.critic_dim))
+        self.state_mem = np.zeros((self.mem_size, self.critic_dims))
+        self.new_state_mem = np.zeros((self.mem_size, self.critic_dims))
         self.reward_mem = np.zeros((self.mem_size, self.n_agents))
         self.terminal_mem = np.zeros((self.mem_size, self.n_agents), dtype=bool) # terminal mask
 
@@ -27,12 +27,12 @@ class Replay:
         self.actor_action_mem = []
 
         for i in range(self.n_agents):
-            self.actor_state_mem.append(np.zeros((self.mem_size, self.actor_dim[i])))
-            self.actor_new_state_mem.append(np.zeros((self.mem_size, self.actor_dim[i])))
+            self.actor_state_mem.append(np.zeros((self.mem_size, self.actor_dims[i])))
+            self.actor_new_state_mem.append(np.zeros((self.mem_size, self.actor_dims[i])))
             self.actor_action_mem.append(np.zeros((self.mem_size, self.n_actions[i])))
 
     def store_transition(self, obs, state, action, reward, obs_, state_, done):
-        index = self.mem_cntr % self.mem_size
+        index = self.mem_cntr % self.mem_size # position of the first available memory
         self.state_mem[index] = state # concat observation of all the agents
         self.new_state_mem[index] = state_
         self.reward_mem[index] = reward
